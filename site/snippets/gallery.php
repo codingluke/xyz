d29adaa0967311830cd305ad14e9f2911f1392de
codingluke <lukashodel@gmail.com>
2014-05-17 15:52:10 +0200

    Ignore preview image in Gallery

--- site/snippets/gallery.php
+++ site/snippets/gallery.php
@@ -16,11 +16,15 @@
     return $result;
   }
   $index = 1;
+  $imgs = array();
+  foreach($page->images() as $img)
+    if ($img->name() != 'preview')
+      $imgs[] = $img;
 ?>
 
 <div class="module">
   <div class="litegallery">
-    <?php foreach($page->images() as $img): ?>
+    <?php foreach($imgs as $img): ?>
     <?php if (in_arith_serie(1, 3, $index)) { ?>
       <div class="litegallery__row--one">
         <img class="litegallery__img1" src="<?php echo thumb($img, array('width' => 1032, 'height' => 500, 'crop' => true), false) ?>" />
