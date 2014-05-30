06753a08b253a36b2a0b4b74814a632b5552dce6
codingluke <lukashodel@gmail.com>
2014-05-30 08:51:28 +0200

    Adds standard viewport

--- site/templates/default.php
+++ site/templates/default.php
@@ -10,7 +10,6 @@
     <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
     <meta name="viewport" content="width=device-width">
     <meta name="robots" content="index, follow" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, target-densityDpi=device-dpi" />
 
     <link rel="shortcut icon" href="/favicon.ico">
     <?php echo css('assets/styles/main.css') ?>
